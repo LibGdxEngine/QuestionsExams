5b112a3a8427ac24b909dd97ccf73b3618e74004
Ahmed Fathy <a.ramadan.ext@tahaluf.ai>
2024-05-15 09:32:09 +0300

    updated

--- core_apps/questions/admin.py
+++ core_apps/questions/admin.py
@@ -14,6 +14,21 @@ class QuestionAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     ordering = ('text', 'language', 'specificity', 'level', 'years', 'subjects', 'systems', 'topics', 'answers',
                 'correct_answer', 'is_used', 'is_correct')
 
+    def get_years(self, obj):
+        return ", ".join([str(year) for year in obj.years.all()])
+
+    def get_subjects(self, obj):
+        return ", ".join([str(subject) for subject in obj.subjects.all()])
+
+    def get_systems(self, obj):
+        return ", ".join([str(system) for system in obj.systems.all()])
+
+    def get_topics(self, obj):
+        return ", ".join([str(topic) for topic in obj.topics.all()])
+
+    def get_correct_answer(self, obj):
+        return str(obj.correct_answer)
+
 
 admin.site.register(Language)
 admin.site.register(Specificity)
