256c77308fc214a28709f861442c9cf1965832e0
Ahmed Fathy <a.ramadan.ext@tahaluf.ai>
2024-05-15 09:48:26 +0300

    updates

--- core_apps/questions/resources.py
+++ core_apps/questions/resources.py
@@ -1,11 +1,20 @@
 from import_export import resources, fields
-from .models import Question
+from import_export.widgets import ForeignKeyWidget
+from import_export import fields, resources
+
+from .models import Question, Language
 
 
 class QuestionResource(resources.ModelResource):
+    language = fields.Field(
+        attribute='related_field',
+        widget=ForeignKeyWidget(Language, 'name'),  # Customize lookup field
+    )
+
     class Meta:
         model = Question
-        fields = ('id', 'text', 'language', 'specificity', 'level', 'years', 'subjects', 'systems', 'topics', 'answers',
+        fields = ('id', 'text', 'specificity', 'level', 'years', 'subjects', 'systems', 'topics', 'answers',
                   'correct_answer', 'is_used', 'is_correct')
+
         import_id_fields = ['id']  # Field used to match existing objects during import
         skip_unchanged = True
