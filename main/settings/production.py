37b0792ea7e150ef627971cec4f01c6e9d390b8e
LibGdxEngine <libgdxengine@gmail.com>
2024-10-18 17:57:01 +0300

    fix email

--- main/settings/production.py
+++ main/settings/production.py
@@ -57,7 +57,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'letaskono.app2@gmail.com'
+EMAIL_HOST_USER = 'krokplus313@gmail.com'
 EMAIL_HOST_PASSWORD = 'rqqy frdv yyfe hmtc'
 DEFAULT_FROM_EMAIL = 'krokplus313@gmail.com'
 DOMAIN = env("DOMAIN", default='krokplus.com')
