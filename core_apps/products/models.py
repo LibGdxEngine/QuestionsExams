cbf6cc480349fcaeae20f3fd071fb8ceb21fbc2b
LibGdxEngine <libgdxengine@gmail.com>
2025-05-07 23:31:01 +0300

    add many images

--- core_apps/products/models.py
+++ core_apps/products/models.py
@@ -6,6 +6,17 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     img = models.ImageField(upload_to='products/', null=True, blank=True)
+    category = models.CharField(max_length=100, null=True, blank=True)
+    product_images = models.ManyToManyField('ProductImage', blank=True, related_name='products')
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to='product_images/')
+    alt_text = models.CharField(max_length=200, blank=True)
+    is_feature = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image for {self.products.first().name if self.products.exists() else 'No Product'}"
 
     def __str__(self):
         return self.name
--- core_apps/products/serializers.py
+++ core_apps/products/serializers.py
@@ -4,4 +4,4 @@ from core_apps.products.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'img']
+        fields = ['id', 'name', 'description', 'price', 'img', 'category', 'product_images']
