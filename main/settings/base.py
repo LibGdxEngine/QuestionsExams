31ad546f0a4d4ec9bbe612bf2ef93554651071f8
LibGdx <libgdxengine@gmail.com>
2025-07-20 14:38:22 +0300

    fix CORS

--- main/settings/base.py
+++ main/settings/base.py
@@ -253,7 +253,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_URLS_REGEX = r"^api/.*$"
+CORS_URLS_REGEX = r"^/api/.*$"
+
 
 AUTH_USER_MODEL = "users.User"
 
