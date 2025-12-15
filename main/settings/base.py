5aa62b685e0c937eed01872a6960cd1e024916d2
LibGdxEngine <libgdxengine@gmail.com>
2025-12-15 23:59:47 +0200

    fix logout

--- main/settings/base.py
+++ main/settings/base.py
@@ -276,9 +276,9 @@ if USE_TZ:
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
